4f664c4f667ae0f70112202ac094bfbff4d17b8f
Jisu Han <weblite@naver.com>
2022-01-16 00:34:00 +0900

    request 메세지 다루기

--- src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -3,6 +3,7 @@ package hello.servlet.basic.response;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,8 +25,35 @@ public class ResponseHeaderServlet extends HttpServlet {
 
         //[Header 편의 메서드] content(response); cookie(response); redirect(response);
         //[message body]
+        content(response);
+        cookie(response);
+        redirect(response);
 
         PrintWriter writer = response.getWriter();
         writer.println("ok");
+    }
+
+    private void content(HttpServletResponse response) {
+        //Content-Type: text/plain;charset=utf-8
+        // Content-Length: 2
+        // response.setHeader("Content-Type", "text/plain;charset=utf-8");
+         response.setContentType("text/plain");
+         response.setCharacterEncoding("utf-8"); //response.setContentLength(2); //(생략시 자동 생성)
+
+    }
+    public void cookie(HttpServletResponse response){
+        // Set-Cookie: myCookie=good; Max-Age=600;
+        // response.setHeader("Set-Cookie", "myCookie=good; Max-Age=600");
+        Cookie cookie=new Cookie("myCookie","good");
+        cookie.setMaxAge(600); // 600초의 생명
+        response.addCookie(cookie);
+    }
+    private void redirect(HttpServletResponse response) throws IOException {
+        //Status Code 302
+        //Location: /basic/hello-form.html
+
+        //response.setStatus(HttpServletResponse.SC_FOUND); //302
+        //response.setHeader("Location", "/basic/hello-form.html");
+        response.sendRedirect("/basic/hello-form.html");
     }
 }
